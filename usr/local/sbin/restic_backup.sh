d30f66d4a9291570a64037714bece96ade301a5a
Tyler Westland <tylerofthewest@gmail.com>
2020-04-06 18:12:47 -0400

    Removes B2 stuff.
    I didn't want to use that, so I deleted all mentions of it.

--- usr/local/sbin/restic_backup.sh
+++ usr/local/sbin/restic_backup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Make backup my system with restic to Backblaze B2.
+# Make backup my system with restic
 # This script is typically run by: /etc/systemd/system/restic-backup.{service,timer}
 
 # Exit on failure, pipe failure
@@ -22,8 +22,8 @@ RETENTION_MONTHS=18
 RETENTION_YEARS=3
 
 # What to backup, and what to not
-BACKUP_PATHS="/ /boot /home"
-[ -d /mnt/media ] && BACKUP_PATHS+=" /mnt/media"
+BACKUP_PATHS="/home/*/.quodlibet /home/*/.radare2rc /home/*/.ssh /home/*/Documents /home/*/Development /home/*/Drive /home/*/Games /home/*/Music /home/*/Pictures /home/*/Templates /home/*/Videos /home/*/VirutalBox /home/*/Zotero /home/git/repos"
+
 BACKUP_EXCLUDES="--exclude-file /etc/restic/backup_exclude"
 for dir in /home/*
 do
@@ -38,10 +38,7 @@ BACKUP_TAG=systemd.timer
 
 # Set all environment variables like
 # B2_ACCOUNT_ID, B2_ACCOUNT_KEY, RESTIC_REPOSITORY etc.
-source /etc/restic/b2_env.sh
-
-# How many network connections to set up to B2. Default is 5.
-B2_CONNECTIONS=50
+source /etc/restic/restic_env.sh
 
 # NOTE start all commands in background and wait for them to finish.
 # Reason: bash ignores any signals while child process is executing and thus my trap exit hook is not triggered.
@@ -60,7 +57,6 @@ restic backup \
 	--verbose \
 	--one-file-system \
 	--tag $BACKUP_TAG \
-	--option b2.connections=$B2_CONNECTIONS \
 	$BACKUP_EXCLUDES \
 	$BACKUP_PATHS &
 wait $!
